6a349cec1516670a61378de0892c0b65ddc4838f
Justin Le <justin@jle.im>
2016-10-25 20:52:38 -0700

    oneHot and argMax/argMins

--- src/TensorOps/Tensor.hs
+++ src/TensorOps/Tensor.hs
@@ -18,21 +18,24 @@ module TensorOps.Tensor
   , zip3
   , add
   , gradLift
-  -- , gradLift1
-  -- , gradLift1N
   , inner, outer, outerV, dot, matVec, vecMat, matMat
   , fromList, generate, rows, toRows
   , ixElems, elems, itoList, toList, unScalar
+  , oneHot, argMax, argMin
   ) where
 
+-- import           Data.List.Util
+-- import           Numeric.AD
+-- import           Type.Family.Nat
+-- import qualified Data.Foldable                 as F
 import           Control.Applicative
 import           Control.Monad.Trans.State.Strict
 import           Data.Functor
 import           Data.Kind
 import           Data.List hiding                 ((\\), zip, map, zip3)
-import           Data.List.Util
 import           Data.Monoid
 import           Data.Proxy
+import           Data.Semigroup
 import           Data.Singletons
 import           Data.Type.Combinator
 import           Data.Type.Fin
@@ -41,16 +44,14 @@ import           Data.Type.Product hiding         (toList)
 import           Data.Type.Sing
 import           Data.Type.Vector                 as TCV
 import           Data.Type.Vector.Util            as TCV
-import           Numeric.AD
 import           Prelude hiding                   (zip, map, zip3)
 import           TensorOps.NatKind
 import           TensorOps.Types
+import           Type.Class.Higher
 import           Type.Class.Known
-import           Type.Class.Witness hiding        (inner, outer)
+import           Type.Class.Witness hiding        (inner, outer, Const)
 import           Type.Family.List
 import           Type.Family.List.Util
-import           Type.Family.Nat
-import qualified Data.Foldable                    as F
 
 konst
     :: (Tensor t, SingI n)
@@ -119,84 +120,7 @@ gradLift fs xs dtdys =
         -> ElemT t
     go i x dtdy = sumV $ (vap . liftA2) (\d f -> d * index' i (vfGrad f x)) dtdy fs
     {-# INLINE go #-}
--- {-# INLINE[0] gradLift #-}
-
--- naiveGradLift
---     :: forall o n m t. (Tensor t, Floating (ElemT t), SingI o)
---     => Vec m (VFunc n)
---     -> Vec n (t o)    -- ^ inputs
---     -> Vec m (t o)    -- ^ d target / d outputs
---     -> Vec n (t o)    -- ^ d target / d inputs
--- naiveGradLift fs xs dtdys =
---     liftT (vgen_ (\i -> I (uncurry (go i) . splitVec known)))
---           (xs `TCV.append'` dtdys)
---       \\ xs
---   where
---     go  :: Fin n
---         -> Vec n (ElemT t)
---         -> Vec m (ElemT t)
---         -> ElemT t
---     go i x dtdy = sum $ (vap . liftA2) (\d (VF f) -> d * index' i (grad f x)) dtdy fs
---     {-# INLINE go #-}
-
--- {-# RULES
--- "gradLift/1"   gradLift = gradLift1'
--- "gradLift/1N"  gradLift = gradLift1N'
---   #-}
-
--- -- | 'gradLift' specialized for 'N1'.
--- gradLift1'
---     :: forall t o. (Tensor t, SingI o, Floating (ElemT t))
---     => Vec N1 (VFunc N1)
---     -> Vec N1 (t o)
---     -> Vec N1 (t o)
---     -> Vec N1 (t o)
--- gradLift1' fs xs dtdys =
---     case fs of
---       I f :* ØV -> case xs of
---         I x :* ØV -> case dtdys of
---           I dtdy :* ØV -> gradLift1 (getVF f . (:* ØV) . I) x dtdy
-
--- -- | 'gradLift' specialized for 'N1' -> m.
--- gradLift1N'
---     :: forall t m o. (Tensor t, SingI o, Floating (ElemT t))
---     => Vec m (VFunc N1)
---     -> Vec N1 (t o)
---     -> Vec m (t o)
---     -> Vec N1 (t o)
--- gradLift1N' fs xs dtdys =
---     case xs of
---       I x :* ØV -> gradLift1N fs x dtdys
-
--- -- | A faster version of 'gradLift' using forward-mode AD
--- gradLift1
---     :: forall t o. (Tensor t, SingI o, Floating (ElemT t))
---     => (forall a. Floating a => a -> a)
---     -> t o
---     -> t o
---     -> Vec N1 (t o)
--- gradLift1 f x dtdy =
---     liftT (I (\(I x' :* I d :* ØV) -> d * f x') :* ØV)
---           (I x :* I dtdy :* ØV)
-
--- -- | A faster version of 'gradLift' using forward-mode AD, with multiple
--- -- outputs.
--- gradLift1N
---     :: forall t m o. (Tensor t, SingI o, Floating (ElemT t))
---     => Vec m (VFunc N1)
---     -> t o
---     -> Vec m  (t o)
---     -> Vec N1 (t o)
--- gradLift1N fs x dtdys =
---     liftT (I (\(I x' :* ds) -> go x' ds) :* ØV)
---           (I x :* dtdys)
---   where
---     go  :: ElemT t
---         -> Vec m (ElemT t)
---         -> ElemT t
---     go x' dtdy = sum $ (vap . liftA2) (\d (VF f) -> d * diff (f . (:* ØV) . I) x') dtdy fs
---     {-# INLINE go #-}
-
+{-# INLINE gradLift #-}
 
 inner
     :: forall t ms ns o. (Tensor t, SingI (o ': ns), SingI (ms ++ ns))
@@ -264,6 +188,7 @@ generate
     => (Prod (IndexN k) ns -> ElemT t)
     -> t ns
 generate = getI . generateA . fmap I
+{-# LANGUAGE generate #-}
 
 rows
     :: (Tensor t, Applicative f, SingI (ms ++ os))
@@ -273,6 +198,7 @@ rows
     -> t (ms ++ ns)
     -> f (t (ms ++ os))
 rows lM lO f = ixRows lM lO (\_ -> f)
+{-# LANGUAGE rows #-}
 
 toRows
     :: (Tensor t, SingI n)
@@ -282,6 +208,7 @@ toRows = ($[])
        . appEndo
        . getConst
        . rows (LS LZ) LZ (\xs -> Const $ Endo (xs:))
+{-# LANGUAGE toRows #-}
 
 ixElems
     :: forall k f (t :: [k] -> Type) ns. (Applicative f, Tensor t, SingI ns)
@@ -295,12 +222,21 @@ ixElems f = (\\ appendNil l) $
     l :: Length ns
     l = singLength sing
 
+ifoldMapElems
+    :: forall k m (t :: [k] -> Type) ns. (Monoid m, Tensor t, SingI ns)
+    => (Prod (IndexN k) ns -> ElemT t -> m)
+    -> t ns
+    -> m
+ifoldMapElems f = getConst . ixElems (\i -> Const . f i)
+{-# INLINE ifoldMapElems #-}
+
 elems
     :: (Applicative f, Tensor t, SingI ns)
     => (ElemT t -> f (ElemT t))
     -> t ns
     -> f (t ns)
 elems f = ixElems (\_ x -> f x)
+{-# INLINE elems #-}
 
 itoList
     :: forall k (t :: [k] -> Type) ns. (Tensor t, SingI ns)
@@ -310,6 +246,7 @@ itoList = ($ [])
         . appEndo
         . getConst
         . ixElems (\i x -> Const $ Endo ((i,x):))
+{-# LANGUAGE itoList #-}
 
 toList
     :: (Tensor t, SingI ns)
@@ -319,9 +256,59 @@ toList = ($[])
        . appEndo
        . getConst
        . elems (\x -> Const $ Endo (x:))
+{-# LANGUAGE toList #-}
 
 unScalar
     :: forall t. Tensor t
     => t '[]
     -> ElemT t
 unScalar = (! Ø)
+{-# LANGUAGE unScalar #-}
+
+oneHot
+    :: forall k (t :: [k] -> Type) (n :: k).
+     ( Tensor t
+     , Eq1 (IndexN k)
+     , SingI n
+     )
+    => ElemT t
+    -> ElemT t
+    -> IndexN k n
+    -> t '[n]
+oneHot x y i = generate $ \(j :< Ø) ->
+                 if j `eq1` i
+                   then x
+                   else y
+{-# LANGUAGE oneHot #-}
+
+argMax
+    :: forall k (t :: [k] -> Type) (n :: k) (p :: k -> Type).
+     ( SingI (Succ n)
+     , Tensor t
+     , Ord (ElemT t)
+     )
+    => p n
+    -> t '[Succ n]
+    -> IndexN k (Succ n)
+argMax _ = (\case Arg _ i -> i)
+         . getMax
+         . option (error "TensorOps.Tensor.argMax: Impossible!") id
+         . ifoldMapElems @k @(Option (Max (Arg (ElemT t) (IndexN k (Succ n)))))
+             (\(j :< Ø) x -> Option . Just . Max $ Arg x j)
+{-# LANGUAGE argMax #-}
+
+argMin
+    :: forall k (t :: [k] -> Type) (n :: k) (p :: k -> Type).
+     ( SingI (Succ n)
+     , Tensor t
+     , Ord (ElemT t)
+     )
+    => p n
+    -> t '[Succ n]
+    -> IndexN k (Succ n)
+argMin _ = (\case Arg _ i -> i)
+         . getMin
+         . option (error "TensorOps.Tensor.argMax: Impossible!") id
+         . ifoldMapElems @k @(Option (Min (Arg (ElemT t) (IndexN k (Succ n)))))
+             (\(j :< Ø) x -> Option . Just . Min $ Arg x j)
+{-# LANGUAGE argMin #-}
